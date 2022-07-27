07626a87b0ac9641e646bb60a17d86bf95cb0feb
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-07-27 09:20:04 +0200

    Clean up CDK

---
+++ 2-hello-cdk/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -0,0 +1,19 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.sns.Topic;
+import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
+import software.amazon.awscdk.services.sqs.Queue;
+
+public class CdkWorkshopStack extends Stack {
+    public CdkWorkshopStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+    }
+}
