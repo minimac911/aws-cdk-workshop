87d96addaccdc850b23121c0e89d4282dcbaa6ea
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-07-27 10:54:47 +0200

    Added lambda using node and adding it to stack

--- 2-hello-cdk/src/main/java/com/myorg/CdkWorkshopStack.java
+++ 2-hello-cdk/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,11 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
-import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -15,5 +14,11 @@ public class CdkWorkshopStack extends Stack {
 
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
+
+        final Function helloLambda = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
+                .build();
     }
 }
