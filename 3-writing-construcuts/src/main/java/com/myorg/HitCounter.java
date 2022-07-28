6c199f90b0d1fee324b6ff05e6e49ba154ba5350
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-07-28 11:04:12 +0200

    Created lambda for hit counter

--- 3-writing-construcuts/src/main/java/com/myorg/HitCounter.java
+++ 3-writing-construcuts/src/main/java/com/myorg/HitCounter.java
@@ -5,6 +5,6 @@ import software.constructs.Construct;
 public class HitCounter extends Construct {
     public HitCounter(final Construct scope, final String id, final HitCounterProps props){
         super(scope, id);
-        
+
     }
 }
