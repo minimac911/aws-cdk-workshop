7e9790f7616c5045b832a0df2b359930f5c726bf
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-07-28 08:55:00 +0200

    Created bass project

---
+++ 3-writing-construcuts/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -0,0 +1,29 @@
+package com.myorg;
+
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+
+public class CdkWorkshopStack extends Stack {
+    public CdkWorkshopStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        final Function helloLambda = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
+                .build();
+
+        LambdaRestApi.Builder.create(this, "Endpoint")
+                .handler(helloLambda)
+                .build();
+    }
+}
