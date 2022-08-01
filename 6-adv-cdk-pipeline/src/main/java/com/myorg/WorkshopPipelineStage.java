ac20a7e7b4508637844da6ff453cf15c77f1d437
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-08-01 08:10:28 +0200

    Added CD test

--- 6-adv-cdk-pipeline/src/main/java/com/myorg/WorkshopPipelineStage.java
+++ 6-adv-cdk-pipeline/src/main/java/com/myorg/WorkshopPipelineStage.java
@@ -1,10 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stage;
 import software.amazon.awscdk.StageProps;
 import software.constructs.Construct;
 
 public class WorkshopPipelineStage extends Stage {
+
+    public final CfnOutput outputViewerUrl;
+    public final CfnOutput outputEndpoint;
+
     public WorkshopPipelineStage(Construct scope, String id) {
         this(scope, id, null);
     }
@@ -12,6 +17,9 @@ public class WorkshopPipelineStage extends Stage {
     public WorkshopPipelineStage(Construct scope, String id, StageProps props) {
         super(scope, id, props);
 
-        new CdkWorkshopStack(this, "WorkshopWebService");
+        final CdkWorkshopStack service = new CdkWorkshopStack(this, "WorkshopWebService");
+
+        outputViewerUrl = service.outputViewerUrl;
+        outputEndpoint = service.outputEndpoint;
     }
 }
