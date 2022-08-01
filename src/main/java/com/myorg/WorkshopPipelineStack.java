d23f918f7b7dcc4629f5b6e737f9026a83e72cf4
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-08-01 08:20:21 +0200

    Added tests for CD

--- src/main/java/com/myorg/WorkshopPipelineStack.java
+++ src/main/java/com/myorg/WorkshopPipelineStack.java
@@ -1,12 +1,11 @@
 package com.myorg;
 
 import java.util.List;
+import java.util.Map;
+
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.pipelines.CodeBuildStep;
-import software.amazon.awscdk.pipelines.CodePipeline;
-import software.amazon.awscdk.pipelines.CodePipelineSource;
-import software.amazon.awscdk.pipelines.ConnectionSourceOptions;
+import software.amazon.awscdk.pipelines.*;
 import software.constructs.Construct;
 
 public class WorkshopPipelineStack extends Stack {
@@ -38,7 +37,24 @@ public class WorkshopPipelineStack extends Stack {
                 .build();
 
         final WorkshopPipelineStage stage = new WorkshopPipelineStage(this, "Deploy");
-        pipeline.addStage(stage);
+        StageDeployment stageDeployment = pipeline.addStage(stage);
+
+        stageDeployment.addPost(
+                CodeBuildStep.Builder.create("TestViewerEndpoint")
+                        .projectName("TestViewerEndpoint")
+                        .commands(List.of("curl -Ssf $ENDPOINT_URL"))
+                        .envFromCfnOutputs(Map.of("ENDPOINT_URL", stage.outputViewerUrl))
+                        .build(),
+                CodeBuildStep.Builder.create("TestAPIGatewayEndpoint")
+                        .projectName("TestAPIGatewayEndpoint")
+                        .commands(List.of(
+                                "curl -Ssf $ENDPOINT_URL",
+                                "curl -Ssf $ENDPOINT_URL/test",
+                                "curl -Ssf $ENDPOINT_URL/hello"
+                        ))
+                        .envFromCfnOutputs(Map.of("ENDPOINT_URL", stage.outputEndpoint))
+                        .build()
+        );
     }
 
 
